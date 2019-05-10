8a34b5a42355453fc51b99f80b49d00b0023ceb6
pevkirill <49532758+pevkirill@users.noreply.github.com>
2019-05-10 19:05:40 +0300

    Add files via upload
    signal analysis txd

--- testbench.sv
+++ testbench.sv
@@ -11,21 +11,26 @@ logic       data_txd      ;
 logic [7:0] data_o        ;
 logic [7:0] data_r        ;
 logic [7:0] busy          ;
-logic [7:0] memory  [11:0];
+localparam SIZE_MEMORY = 12 ;
+logic [7:0] memory  [SIZE_MEMORY];
 
 parameter    XTAL_CLK = 100_000_000,
-			     BAUD = 115_200; 
-localparam HALFPERIOD = 1_000_000_000/XTAL_CLK/2;
+BAUD = 115_200;
+localparam HALFPERIOD      = 1_000_000_000/XTAL_CLK/2;
+localparam STATUS_ADDR_REG = 4'h1                    ;
+localparam TXDATA_ADDR_REG = 4'h0                    ;
 
+logic [7:0] check_uart;///
+localparam DIV = XTAL_CLK/BAUD;///
 /*------------------------------------------------------------------------------
  --  generator clock
- ------------------------------------------------------------------------------*/ 
+ ------------------------------------------------------------------------------*/
 initial begin
-	clk_i = 1'b0;
-	forever #(HALFPERIOD) clk_i=~clk_i;
+    clk_i = 1'b0;
+    forever #(HALFPERIOD) clk_i=~clk_i;
 end
 
-initial begin 
+initial begin
     forever @(posedge clk_i) busy = data_r;
 end
 
@@ -35,61 +40,30 @@ initial begin
     read   = 1'b0;
     data_i = 8'h0;
     address = 4'h1;
-    memory = {8'h48, 8'h45, 8'h4c, 8'h4c, 8'h4f, 8'h5f, 8'h57, 8'h4f, 8'h52, 8'h4c, 8'h44, 8'h21};
+    check_uart = '0;
+    memory = {8'h48, 8'h45, 8'h4c, 8'h89, 8'h4f, 8'h5f, 8'h57, 8'h66, 8'h52, 8'h99, 8'h44, 8'h21};
     $display("Running testbench. Testbench is works on frequency = ", XTAL_CLK, " Hz");
     @(posedge clk_i);
     rst_n = 1'b1;
 
-ReadUart(4'h1, data_r);
- @(posedge clk_i);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
-if   (busy[0]==1) WriteUart(4'h0, data_mem);
-while(busy[0]==1) ReadUart(4'h1, data_r);
-while(busy[0]==0) ReadUart(4'h1, data_r);
+    ReadUart(4'h1, data_r);
 
+    @(posedge clk_i);
+    for (int i = 0; i < SIZE_MEMORY; i++) begin
+        while(busy[0]==0) ReadUart(STATUS_ADDR_REG, data_r);
+        if   (busy[0]==1) WriteUart(TXDATA_ADDR_REG, memory[i]);
+        while(busy[0]==1) ReadUart(STATUS_ADDR_REG, data_r);
+    end
 
-#500000 $display("stop testbench");
-   $stop;
+    #500000 $display("stop testbench");
+    $stop;
 end
 
 task ReadUart;
     input [3:0] addr_r;
     output [7:0] data_r;
 
-    begin 
+    begin
         @(posedge clk_i);
         @(posedge clk_i);
         @(posedge clk_i);
@@ -98,7 +72,7 @@ task ReadUart;
         data_r = data_o;
         @(posedge clk_i);
         read = 1'b0;
-        address = 4'h1;    
+        address = 4'h1;
     end
 
 endtask : ReadUart
@@ -122,42 +96,58 @@ task WriteUart;
 
 endtask : WriteUart
 
+
 initial begin
-forever @(posedge write)
-begin
-    memory[0]  <= memory[11];
-    memory[1]  <= memory[0];
-    memory[2]  <= memory[1];
-    memory[3]  <= memory[2];
-    memory[4]  <= memory[3];
-    memory[5]  <= memory[4];
-    memory[6]  <= memory[5];
-    memory[7]  <= memory[6];
-    memory[8]  <= memory[7];
-    memory[9]  <= memory[8];
-    memory[10] <= memory[9];
-    memory[11] <= memory[10];
+    forever begin
+        @(posedge write);
+        for (int j = 0; j < DIV*10; j++) begin
+            @(posedge clk_i);
+            if (j==DIV*1+2) begin
+                check_uart[7] = data_txd;
+            end
+            if (j==DIV*2+5) begin
+                check_uart[6] = data_txd;
+            end
+            if (j==DIV*3+10) begin
+                check_uart[5] = data_txd;
+            end
+            if (j==DIV*4+15) begin
+                check_uart[4] = data_txd;
+            end
+            if (j==DIV*5+20) begin
+                check_uart[3] = data_txd;
+            end
+            if (j==DIV*6+25) begin
+                check_uart[2] = data_txd;
+            end
+            if (j==DIV*7+30) begin
+                check_uart[1] = data_txd;
+            end
+            if (j==DIV*8+35) begin
+                check_uart[0] = data_txd;
+            end
+            if(j==DIV*9+13 && data_txd) begin
+                for (int g = 0; g < SIZE_MEMORY; g++) begin
+                    if(check_uart == memory[g]) begin
+                        $display("uart data is correct", check_uart);
+                    end 
+                end
+            end
+        end
+    end
 end
-end
-
-logic [7:0] data_mem;
-
-initial begin 
-    forever @(posedge clk_i) data_mem = memory[11];
-end
-
 
 uart_core #(.CLK_FREQ (XTAL_CLK),.BAUD_RATE(BAUD)) uart_core_inst
 (
     .clk_i           (clk_i   ),
     .arst_n_i        (rst_n   ),
-    
+
     .avms_address_i  (address ),
     .avms_read_i     (read    ),
     .avms_write_i    (write   ),
     .avms_writedata_i(data_i  ),
     .avms_readdata_o (data_o  ),
-    
+
     .uart_txd_o      (data_txd)
 );
 
